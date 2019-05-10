dd3b20612cc1d21707c7b2b5eaa550152ac1351b
tarasgl <tarasgl@gmail.com>
2019-05-10 17:00:07 +0300

    added javadoc and some code refactoring

--- src/LongestPalindromicSubsequence.java
+++ src/LongestPalindromicSubsequence.java
@@ -1,15 +1,30 @@
 import java.util.Scanner;
 
+/**
+ * Functionality to find the longest palindromic subsequence in a string.
+ *
+ * <p>Provides functionality to find the longest palindromic subsequence in a string
+ * that is written by a user in console
+ * @author Taras Hlukhovetskiy
+ */
 public class LongestPalindromicSubsequence implements Executable {
 
     private Scanner in;
-    private int numberOfFib;
 
+    /**
+     * Constructor that sets Scanner.
+     * @param in Scanner to get the input from
+     */
     public LongestPalindromicSubsequence(Scanner in) {
         this.in = in;
     }
 
-    // A utility function to get the maximum of two integers
+    /**
+     * A utility function to get the maximum of two integers.
+     * @param x first int
+     * @param y second int
+     * @return int maximum of x and y
+     */
     static int max(int x, int y) {
         return (x > y) ? x : y;
     }
@@ -18,40 +33,49 @@ public class LongestPalindromicSubsequence implements Executable {
     public void execute() {
         System.out.println("Input string to be checked for palindromic subsequence");
         String seq = in.nextLine();
-        System.out.println("The length of the lps is " + lps(seq));
+        System.out.println("The length of the lps is "
+                + findLongestPalindromicSubsequenceLength(seq));
     }
 
-    // Returns the length of the longest
-    // palindromic subsequence in seq
-    public static int lps(String seq) {
-        int n = seq.length();
+
+    /**
+     * Finds the length of the longest palindromic subsequence in the string.
+     * @param sequence string as a sequence
+     * @return int length of the longest palindromic subsequence
+     */
+    public static int findLongestPalindromicSubsequenceLength(String sequence) {
+        int n = sequence.length();
         if (n == 0) {
             return 0;
         }
-        int i, j, cl;
+        int i;
+        int j;
+        int cl;
         // Create a table to store results of subtasks
-        int L[][] = new int[n][n];
+        int[][] subtaskAnswers = new int[n][n];
 
         // Strings of length 1 are palindromes of length 1
-        for (i = 0; i < n; i++)
-            L[i][i] = 1;
-
+        for (i = 0; i < n; i++) {
+            subtaskAnswers[i][i] = 1;
+        }
 
         // cl is length of substring
         for (cl = 2; cl <= n; cl++) {
             for (i = 0; i < n - cl + 1; i++) {
                 j = i + cl - 1;
-                if (seq.charAt(i) == seq.charAt(j))
-                    if (cl == 2)
-                        L[i][j] = 2;
-                    else
-                        L[i][j] = L[i + 1][j - 1] + 2;
-                else
-                    L[i][j] = max(L[i][j - 1], L[i + 1][j]);
+                if (sequence.charAt(i) == sequence.charAt(j)) {
+                    if (cl == 2) {
+                        subtaskAnswers[i][j] = 2;
+                    } else {
+                        subtaskAnswers[i][j] = subtaskAnswers[i + 1][j - 1] + 2;
+                    }
+                } else {
+                    subtaskAnswers[i][j] = max(subtaskAnswers[i][j - 1], subtaskAnswers[i + 1][j]);
+                }
             }
         }
 
-        return L[0][n - 1];
+        return subtaskAnswers[0][n - 1];
     }
 
 }
