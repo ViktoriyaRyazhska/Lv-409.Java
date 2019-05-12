873d82e28f56068461115a5749ee6f8a693900b6
Olena Dirych <diru4ova@gmail.com>
2019-05-12 14:32:48 +0300

    Update InsertionSortTest.java

--- test/InsertionSortTest.java
+++ test/InsertionSortTest.java
@@ -20,7 +20,7 @@ public class InsertionSortTest {
         int[] actual = {9, 8, 7, 6, 9, 6};
         int[] expected = {6, 6, 7, 8, 9, 9};
         sort.insertionSort(actual);
-        assertEquals(Arrays.toString(expected), Arrays.toString(actual));
+        assertArrayEquals(expected, actual);
 
     }
 
